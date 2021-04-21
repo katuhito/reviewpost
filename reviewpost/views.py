221516f7d9552de77eda8d31fccfbc83c582fcc9
Katuhito Hara <katuhitohara@gmail.com>
2021-04-21 11:50:28 +0900

    signup

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-# from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+
 
 def signupview(request):
-    return render(request, 'signup.html', {'somedata':100})
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        try:
+            user = User.objects.create_user(username_data, '', password_data)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error':'このユーザーはすでに登録されています。'})   
+    else:
+        return render(request, 'signup.html', {})
+    return render(request, 'signup.html', {})
